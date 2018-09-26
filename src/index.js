8aad19ad177b9365d58f4e379d7cc1376f97b148
icebergnevercries <denislosik@mail.ru>
2018-09-26 19:47:47 +0300

    implementation function, which calculate how many zeros in the end of faactorial of number in base system

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  const arrayOfPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31];
+  let countZeros = number, arrayForBase = [], arrayForDegree = [], arrayForZeros = [];
+
+  for(let i = 0, len = arrayOfPrimes.length, buff = base, degreeCount = 0; i < len; i++)
+  {
+    if(buff % arrayOfPrimes[i] == 0){      
+      while(buff % arrayOfPrimes[i] == 0){
+        buff /= arrayOfPrimes[i];
+        degreeCount++;
+      }
+      arrayForBase.push(arrayOfPrimes[i]);
+      arrayForDegree.push(degreeCount);
+      degreeCount = 0;
+    }     
+    if(buff != 1 && i == len - 1){
+      arrayForBase.push(buff);
+      arrayForDegree.push(1); 
+    } 
+  }
+
+  for(let i = 0, len = arrayForBase.length, buff = 0, degreeCount = 1; i < len; i++)
+  {
+    while(number > Math.pow(arrayForBase[i], degreeCount)){
+      buff += Math.floor(number / Math.pow(arrayForBase[i], degreeCount));
+      degreeCount++;
+    }
+    arrayForZeros.push(Math.floor(buff/arrayForDegree[i]));
+    buff = 0;
+    degreeCount = 1;         
+  }
+
+  for(let i = 0, len = arrayForZeros.length; i < len; i++){
+    if(countZeros > arrayForZeros[i]){
+      countZeros = arrayForZeros[i];
+    }
+  }
+
+  return countZeros;
 }
